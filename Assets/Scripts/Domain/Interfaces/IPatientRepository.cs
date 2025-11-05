49344c77e5a1db8b9af2dcd715e9ba4ab4a872b6
LandfillHeart <kimbleredward@gmail.com>
2025-11-05 16:47:13 +0100

    Stesura basi domain layer, interfacce e classi
    - Interfacce per astrarre le tabelle, permettendo di comunicare in maniera uguale con database o dati mock in memoria (elemento necessario per Dependency Injection)
    - Classi che rappresentano le singole righe delle tabelle del DB: Dottore, Paziente, Appuntamento.

---
+++ Assets/Scripts/Domain/Interfaces/IPatientRepository.cs
@@ -0,0 +1,11 @@
+
+namespace Appointix.Domain.Interfaces
+{
+	public interface IPatientRepository
+	{
+		public void CreatePatient(string name, string surname, string email, string phoneNumber);
+		public Patient ReadPatient(int id);
+		public void UpdatePatient(int id, Patient newData);
+		public void DeletePatient(int id);
+	}
+}
